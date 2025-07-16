c6b0bfea08855de07dcff97df23f26d87db07693
helamandc <manlehcastillo@gmail.com>
2025-07-16 22:38:46 +1000

    Weather assignment

---
+++ weather/scripts/weather.js
@@ -0,0 +1,5 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.74&lon=6.64&appid=4f82d79e09da1dcc564f9138f5d01dd7';
