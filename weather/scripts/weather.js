3845c451da25bf0307bcf87f5312158f11dab9ba
helamandc <manlehcastillo@gmail.com>
2025-07-17 00:45:06 +1000

    Removed env and git ignore

--- weather/scripts/weather.js
+++ weather/scripts/weather.js
@@ -2,7 +2,7 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-const apiKey = API_KEY;
+const apiKey = API_KEY; //from Open Weather API Key
 
 const url = `https://api.openweathermap.org/data/2.5/weather?lat=49.74&lon=6.64&appid=${apiKey}`;
 
