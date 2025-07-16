dbde75e1d5cc70f829349e711e6d9da2061afcc4
helamandc <manlehcastillo@gmail.com>
2025-07-17 00:30:01 +1000

    Git ignore file

--- weather/scripts/weather.js
+++ weather/scripts/weather.js
@@ -2,7 +2,7 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.74&lon=6.64&appid=4f82d79e09da1dcc564f9138f5d01dd7';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.74&lon=6.64&appid=your_secret_key_here';
 
 async function apiFetch() {
     try {
