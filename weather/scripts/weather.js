df336b0c04069a5bef162d431c0bdaa9d664c117
helamandc <manlehcastillo@gmail.com>
2025-07-17 00:39:39 +1000

    Delete git ignore

--- weather/scripts/weather.js
+++ weather/scripts/weather.js
@@ -2,7 +2,9 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.74&lon=6.64&appid=your_secret_key_here';
+const apiKey = API_KEY;
+
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=49.74&lon=6.64&appid=${apiKey}`;
 
 async function apiFetch() {
     try {
