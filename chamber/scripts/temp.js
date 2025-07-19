8db6b7c1aed31e9cc7ffbc5ae7028f92c3f550fe
helamandc <manlehcastillo@gmail.com>
2025-07-19 23:03:15 +1000

    Week 3 home page

---
+++ chamber/scripts/temp.js
@@ -0,0 +1,33 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const apiKey = '4f82d79e09da1dcc564f9138f5d01dd7'; //from Open Weather API Key
+
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=49.74&lon=6.64&appid=${apiKey}`;
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); // testing only
+            displayResults(data); // uncomment when ready
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('SRC', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
